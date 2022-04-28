df93a134851e4fbf1cddbd4465b9ceb955fc59d2
StuartGavidia <stuarta0403@hotmail.com>
2022-04-27 23:19:51 -0700

    Style Intro Page

--- src/Pages/IntroPage/IntroPage.jsx
+++ src/Pages/IntroPage/IntroPage.jsx
@@ -4,8 +4,24 @@ import "./IntroPage.css"
 export default function IntroPage(){
     return (
         <div className="intro">
-            <h1>Welcome to PLU Science!</h1>
-            <h2>Start Learning in the Learn Page</h2>
+            <div className="intro-banner">
+                <div className="intro-banner-text">
+                    <h1>PLU Learn</h1>
+                    <h2>Start Learning in the Learn Page</h2>
+                    <p>This site is not affiliated with PLU but offers practice problems to solve for anyone.</p>
+                </div>
+            </div>
+            <div className="intro-information">
+                <h1>Future Updates</h1>
+                <div>
+                    <ul>
+                        <li>Adding other subjects</li>
+                        <li>Adding Functionality to Contact Page</li>
+                        <li>Add own sections to practice problems</li>
+                    </ul>
+                </div>
+            </div>
+            
         </div>
     )
 }
