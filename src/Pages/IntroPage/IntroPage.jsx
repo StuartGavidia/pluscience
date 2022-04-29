d3beeb8ad14c26f622d7b906e28fd630725a6a3b
StuartGavidia <stuarta0403@hotmail.com>
2022-04-28 21:10:33 -0700

    Add PLU Info Add Weather Updates

--- src/Pages/IntroPage/IntroPage.jsx
+++ src/Pages/IntroPage/IntroPage.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./IntroPage.css"
+//import { weather } from '../../weather'
 
 export default function IntroPage(){
+    
+    const [weatherData, setWeatherData] = useState();
+
+    function getData() {
+
+        const key = "97b866a027964a528bc15641222904";
+        const city = "Tacoma"
+        const baseUrl = "http://api.weatherapi.com/v1"
+        const method = "/current.json"
+        const alert = "yes"
+    
+        const endpoint = `${baseUrl}${method}?key=${key}&q=${city}&alerts=${alert}`
+    
+        fetch(endpoint).then(response => {
+            return response.json()
+        }, netWorkError => {
+            console.log(netWorkError)
+        }).then(jsonResponse => {
+            console.log(jsonResponse)
+            setWeatherData(jsonResponse.current)
+        })
+    }
+
+    useEffect(() => {
+        getData();
+    }, [])
+
     return (
         <div className="intro">
             <div className="intro-banner">
@@ -10,6 +38,16 @@ export default function IntroPage(){
                     <h2>Start Learning in the Learn Page</h2>
                     <p>This site is not affiliated with PLU but offers practice problems to solve for anyone.</p>
                 </div>
+            </div>
+            <div className="intro-plu">
+            {weatherData ? 
+            <div className="intro-plu-info">
+                <h1>Weather at PLU</h1>
+                <h2>{weatherData.condition.text}</h2>
+                <img src ={weatherData.condition.icon} height="100" width="100" alt={weatherData.condition.text}/>
+                <h2>{weatherData.temp_f} degrees Farenheit</h2>
+            </div>
+            : <h1>Loading</h1>}
             </div>
             <div className="intro-information">
                 <h1>Future Updates</h1>
