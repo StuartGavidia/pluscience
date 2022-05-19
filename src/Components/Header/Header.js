fadbe843db439ced790307edafacd92f51faa0f6
StuartGavidia <stuarta0403@hotmail.com>
2022-05-18 17:38:50 -0700

    Make Mobile Friendly Add Icons Limit sizing of choice components

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -18,11 +18,16 @@ export default function Header({toggleDark, darkMode}) {
                     </div>
                     }
                 </div>
-                <nav className={darkMode ? "dark-header-nav" : "header-nav"}>
-                    <NavLink exact to="/" activeClassName={darkMode ? "dark-header-link-active" : "header-link-active"} className={darkMode ? "dark-header-link" : "header-link"}>Home</NavLink>
-                    <NavLink to="/Learn" activeClassName={darkMode ? "dark-header-link-active" : "header-link-active"} className={darkMode ? "dark-header-link" : "header-link"}>Learn</NavLink>
-                    <NavLink exact to="/Contact" activeClassName={darkMode ? "dark-header-link-active" : "header-link-active"} className={darkMode ? "dark-header-link" : "header-link"}>Contact</NavLink>
+                <nav className={darkMode ? "dark-header-nav links" : "header-nav links"}>
+                    <NavLink exact to="/" activeClassName={darkMode ? "dark-header-link-active" : "header-link-active"} className={darkMode ? "dark-header-link desktop-link" : "header-link desktop-link"}>Home</NavLink>
+                    <NavLink to="/Learn" activeClassName={darkMode ? "dark-header-link-active" : "header-link-active"} className={darkMode ? "dark-header-link desktop-link" : "header-link desktop-link"}>Learn</NavLink>
+                    <NavLink exact to="/Contact" activeClassName={darkMode ? "dark-header-link-active" : "header-link-active"} className={darkMode ? "dark-header-link desktop-link" : "header-link desktop-link"}>Contact</NavLink>
+                    
+                    <NavLink exact to="/" activeClassName="mobile-active" className="mobile-link"><img className="link-img" src="/Assets/Images/home-icon.png" alt="house icon"/></NavLink>
+                    <NavLink to="/Learn" activeClassName="mobile-active" className="mobile-link"><img className="link-img" src="/Assets/Images/learn-icon.png" alt="person reading a book icon"/></NavLink>
+                    <NavLink exact to="/Contact" activeClassName="mobile-active" className="mobile-link"><img className="link-img"src="/Assets/Images/contact-icon.png" alt="email icon"/></NavLink>
                 </nav>
+                    
                 
         </header>
     )
